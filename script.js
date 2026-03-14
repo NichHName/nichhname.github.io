bf9afdb2de47fb06919e143b3e2e94fb9cd6cef9
Nicholas Harsell <nwilsonh1201@gmail.com>
2026-03-13 20:30:56 -0400

    testing perlin background

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
     
     console.log("Website loaded successfully!");
 });
+
+window.addEventListener('scroll', function() {
+    let scrolled = this.window.scrollY;
+
+    let speedMultiplier = 0.2;
+
+    this.document.body.style.backgroundPositionY = (scrolled * speedMultiplier) + 'px';
+});
